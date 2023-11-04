e3d7809d914ee3dfe0a7320583f27829a326ed21
Alex Gold <88856796+goonyboy@users.noreply.github.com>
2023-11-04 16:48:29 +0300

    Add files via upload

---
+++ scripts/script.js
@@ -0,0 +1,45 @@
+const lightTheme = "styles/light.css";
+const darkTheme = "styles/dark.css";
+const sunIcon = "assets/SunIcon.svg";
+const moonIcon = "assets/MoonIcon.svg";
+const themeIcon = document.getElementById("theme-icon");
+const res = document.getElementById("result");
+const toast = document.getElementById("toast");
+
+//Swaps the stylesheet to dark theme
+
+function changeTheme() {
+  const theme = document.getElementById("theme");
+  setTimeout(() => {
+    toast.innerHTML = "Calculator";
+  }, 1500);
+  if (theme.getAttribute("href") === lightTheme) {
+    theme.setAttribute("href", darkTheme);
+    themeIcon.setAttribute("src", sunIcon);
+    toast.innerHTML = "Dark Mode 🌙";
+  } else {
+    theme.setAttribute("href", lightTheme);
+    themeIcon.setAttribute("src", moonIcon);
+    toast.innerHTML = "Light Mode ☀️";
+  }
+}
+
+//Displays entered value on screen
+function calculate(value) {
+    const calculatedValue = eval(value || null);
+    if (isNaN(calculatedValue)) {
+      res.value = "Can't divide 0 with 0";
+      setTimeout(() => {
+        res.value = "";
+      }, 1300);
+    } else {
+      res.value = calculatedValue;
+    }
+  }
+
+function liveScreen(enteredValue) {
+  if (!res.value) {
+    res.value = "";
+  }
+  res.value += enteredValue;
+}
